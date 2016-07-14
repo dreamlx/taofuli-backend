11a24738bc5bff42fee4659d8b636176080b070b
ken-wong <wangkun025@gmail.com>
2016-07-14 14:56:03 +0800

    Deploy set changed

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -20,7 +20,9 @@
 # role :app, %w{deploy@example.com}, my_property: :my_value
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
-
+role :app, %w{deploy@139.196.233.121}
+role :web, %w{deploy@139.196.233.121}
+role :db,  %w{deploy@139.196.233.121}
 
 
 # Configuration
