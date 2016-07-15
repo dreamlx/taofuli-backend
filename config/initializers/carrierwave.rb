505d5f41c300b7ba84b585bb927ddce3da267495
ken-wong <wangkun025@gmail.com>
2016-07-15 09:44:03 +0800

    Use qiniu

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,14 +2,14 @@
   config.storage             = :qiniu
   config.qiniu_access_key    = Rails.application.secrets.qiniu_access
   config.qiniu_secret_key    = Rails.application.secrets.qiniu_secret
-  config.qiniu_bucket        = "sample"
-  config.qiniu_bucket_domain = ""
+  config.qiniu_bucket        = "taofuli"
+  config.qiniu_bucket_domain = "oabwzs795.bkt.clouddn.com"
   # config.qiniu_bucket_private= true #default is false
   config.qiniu_block_size    = 4*1024*1024
   config.qiniu_protocol      = "http"
 end
 
-if Rails.env.test? or Rails.env.cucumber?
+if Rails.env.test? or Rails.env.development? or Rails.env.cucumber?
   CarrierWave.configure do |config|
     config.storage = :file
     config.enable_processing = false
