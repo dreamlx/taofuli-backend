8b0ab13d0c5a110a9d4dcfbc706a193cf229fead
ken-wong <wangkun025@gmail.com>
2016-08-01 20:02:23 +0800

    Fix order creation

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,11 +4,25 @@ class Order < ActiveRecord::Base
   validates :user_id, presence: true
   validates :event, presence: true
   validate :available_quota
+  before_create :set_date
+
+  state_machine :state, :initial => :'未派发' do
+    event :confirm do
+      transition :'未派发' => :'待派发'
+    end
+    event :pay do
+      transition :'待派发' => :'已派发'
+    end
+  end
 
   private
     def available_quota
       if event.available_quota <= 0
         errors.add(:base, "没有名额")
       end
+    end
+
+    def set_date
+      self.date = Date.today
     end
 end
